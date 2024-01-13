738593e91ddc3761c3d9ab6ffb9420fd69a0c656
JamesIsWack <JamesIsWack@github.com>
2024-01-12 23:34:43 -0600

    im so dumb

--- wor_tests/prx.cpp
+++ wor_tests/prx.cpp
@@ -48,8 +48,7 @@ extern "C" uint64_t strlen(const char *s) {
 
 extern "C" int _wor_tests_prx_entry(void)
 {
-
-
+	// enable_button_cheats | for debug menu
 	Script::CSymbolTableEntry* enable_button_cheats_symbol = Script::Resolve(720971780);
 	printf("enable_button_cheats symbol: %p\n", enable_button_cheats_symbol);
 
@@ -57,7 +56,23 @@ extern "C" int _wor_tests_prx_entry(void)
 		printf("enable_button_cheats symbol data: %p %d %d\n", enable_button_cheats_symbol->union_type, enable_button_cheats_symbol->type, enable_button_cheats_symbol->sourceFileNameChecksum);
 		enable_button_cheats_symbol->union_type = 1;
 	}
+	// debug_use_screen_noise | post processing FX disable
+	Script::CSymbolTableEntry* debug_use_screen_noise_symbol = Script::Resolve(3786639802);
+	printf("debug_use_screen_noise symbol: %p\n", debug_use_screen_noise_symbol);
+	
+	if (debug_use_screen_noise_symbol) {
+		printf("debug_use_screen_noise symbol data: %p %d %d\n", debug_use_screen_noise_symbol->union_type, debug_use_screen_noise_symbol->type, debug_use_screen_noise_symbol->sourceFileNameChecksum);
+		debug_use_screen_noise_symbol->union_type = 0;
+	}
+	// debug_use_motion_blur | post processing FX disable
+	Script::CSymbolTableEntry* debug_use_motion_blur_symbol = Script::Resolve(42529484);
+	printf("debug_use_motion_blur symbol: %p\n", debug_use_motion_blur_symbol);
 
+	if (debug_use_motion_blur_symbol) {
+		printf("debug_use_motion_blur symbol data: %p %d %d\n", debug_use_motion_blur_symbol->union_type, debug_use_motion_blur_symbol->type, debug_use_motion_blur_symbol->sourceFileNameChecksum);
+		debug_use_motion_blur_symbol->union_type = 0;
+	}
+	// apply changes to qsymbols
 	CFuncs::RegisterCFuncs();
 
 	// Exit thread using directly the syscall and not the user mode library or else we will crash

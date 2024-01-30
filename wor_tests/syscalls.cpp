a7b8bcaab3d80cc2d0503f88e8c1072c88edbeed
JamesIsWack <JamesIsWack@github.com>
2024-01-29 21:19:16 -0600

    mgviujdigniadhfg

--- wor_tests/syscalls.cpp
+++ wor_tests/syscalls.cpp
@@ -11,12 +11,17 @@ void _sys_ppu_thread_exit(int exit_code) {
 	system_call_1(41,(uint64_t)exit_code);
 }
 
+void _sys_console_write(const char *s, uint32_t len) {
+	system_call_2(398,(uint64_t)s, len);
+}
+
+void _sys_tty_write(uint32_t ch, const void *buf, uint32_t len, uint32_t *pwritelen) {
+	system_call_4(403, (uint64_t)ch, (uint64_t)buf, (uint64_t)len, (uint64_t)pwritelen);
+}
+
 extern "C" {
 	void _putchar(char ch) {
-		//_sys_console_write((char *)&ch, 1);
 		uint32_t write_len = 0;
 		_sys_tty_write(0, &ch, 1, &write_len);
-		//_sys_tty_write(1, &ch, 1, &write_len);
-		//_sys_tty_write(2, &ch, 1, &write_len);
 	}
 }
